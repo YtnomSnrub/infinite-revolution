412d265ce4aea9b7fa415f52b0ad428288294bec
Joel Launder <jalaunder@gmail.com>
2022-08-22 14:37:07 +1000

    Update drive stress format

--- module/token.js
+++ module/token.js
@@ -4,27 +4,29 @@
  */
 export class SimpleTokenDocument extends TokenDocument {
   /** @inheritdoc */
-  getBarAttribute(barName, {alternative}={}) {
-    const data = super.getBarAttribute(barName, {alternative});
+  getBarAttribute(barName, { alternative } = {}) {
+    const data = super.getBarAttribute(barName, { alternative });
     const attr = alternative || this.data[barName]?.attribute;
-    if ( !data || !attr || !this.actor ) return data;
+    if (!data || !attr || !this.actor) return data;
     const current = foundry.utils.getProperty(this.actor.data.data, attr);
-    if ( current?.dtype === "Resource" ) data.min = parseInt(current.min || 0);
+    if (current?.dtype === "Resource") data.min = parseInt(current.min || 0);
     data.editable = true;
+
     return data;
   }
 
   /* -------------------------------------------- */
 
-  static getTrackedAttributes(data, _path=[]) {
-    if ( data || _path.length ) return super.getTrackedAttributes(data, _path);
+  static getTrackedAttributes(data, _path = []) {
+    if (data || _path.length) return super.getTrackedAttributes(data, _path);
     data = {};
-    for ( const model of Object.values(game.system.model.Actor) ) {
+    for (const model of Object.values(game.system.model.Actor)) {
       foundry.utils.mergeObject(data, model);
     }
-    for ( const actor of game.actors ) {
-      if ( actor.isTemplate ) foundry.utils.mergeObject(data, actor.toObject().data);
+    for (const actor of game.actors) {
+      if (actor.isTemplate) foundry.utils.mergeObject(data, actor.toObject().data);
     }
+
     return super.getTrackedAttributes(data);
   }
 }
@@ -39,14 +41,6 @@ export class SimpleTokenDocument extends TokenDocument {
  */
 export class SimpleToken extends Token {
   _drawBar(number, bar, data) {
-    if ( "min" in data ) {
-      // Copy the data to avoid mutating what the caller gave us.
-      data = {...data};
-      // Shift the value and max by the min to ensure that the bar's percentage is drawn accurately if this resource has
-      // a non-zero min.
-      data.value -= data.min;
-      data.max -= data.min;
-    }
     return super._drawBar(number, bar, data);
   }
 }
